380f82faffc0acaf13442d04005c626260c8c808
priyanshuiips2026 <priyanshu.iips2026@gmail.com>
2026-09-17 20:55:52 +0530

    Update Modify operator.c

--- Lab-04/Modify operator.c
+++ Lab-04/Modify operator.c
@@ -1,1 +1,22 @@
 
+#include <stdio.h>
+
+int main() {
+ 
+    int a=5,b;
+    //pre-increament operator
+    b=++a;
+    printf("++a: %d %d\n",a,b);
+    //post-increament operator 
+    b=a++;
+    printf("a++: %d %d\n",a,b);
+    
+    //pre-decrement operator
+    b=--a;
+    printf("--a: %d %d\n",a,b);
+    //post-decrement operator
+    b=a--;
+    printf("a--: %d %d\n",a,b);
+
+    return 0;
+}
