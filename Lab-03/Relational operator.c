0a7ed5fdb2542c88839ad0616419a9557ad86e8f
priyanshuiips2026 <priyanshu.iips2026@gmail.com>
2026-09-17 20:08:10 +0530

    Update Relational operator.c

--- Lab-03/Relational operator.c
+++ Lab-03/Relational operator.c
@@ -1,1 +1,19 @@
 
+include <stdio.h>
+
+int main() {
+    int a=24;
+    int b=68;
+    
+    
+    printf("a<b  : %d\n", a<b);             printf("a>b  : %d\n", a>b);
+    printf("a<=b : %d\n", a<=b);
+    
+    printf("a>=b : %d\n", a>=b);
+    printf("a!=b : %d\n", a!=b);
+    
+    
+    printf("a==b : %d\n", a==b);
+
+    return 0;
+}
